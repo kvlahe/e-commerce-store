8e9c67d58c0244c743e3278212231e021b732524
Kristo Veber <kristoveber7@gmail.com>
2024-02-27 11:03:03 +0200

    Add new components and UI elements

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div className='bg-white border-t'>
+            <div className='mx-auto py-10'>
+                <p className='text-center text-xs text-black'>
+                    &copy; 2023 Store. All rights reserved.
+                </p>
+            </div>
+        </div>
+    )
+}
